7694a9f9404a4d41020792a54947520895900816
kurodamasaki <kurodamasaki@icloud.com>
2021-04-24 16:57:48 +0900

    [Trello (保存機能なし)]を学習しました。

---
+++ 3_semi/week1/object.js
@@ -0,0 +1,15 @@
+const f1 = function(n){
+  return 3 * n + 1
+}
+
+const f2 = function(n){
+  return 10 ** n
+}
+  
+const multiply = function(x,y){
+  return x * y
+}
+
+const plusRound = (x,y){
+  return Math.round(x + y)
+}
