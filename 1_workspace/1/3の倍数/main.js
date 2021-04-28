444abf2d5eb92a103ff8702b376faec39f413a57
kurodamasaki <kurodamasaki@icloud.com>
2021-04-28 15:29:19 +0900

    [3の倍数]を復習しました。

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,10 +1,7 @@
-const genkiFunction = function(number) {
-    // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
-    for (let n = 1; n <= number; n++) {
-      if (n % 3 === 0) {
-      } else if {
-        console.log(n)
-      }
-      }
-    }
-  }
+const number = 24
+
+if (number % 3 === 0) {
+  console.log(number + "!!!!!!!")
+} else {
+  console.log(number)
+}
